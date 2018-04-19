20af6d407274f1afa51eb04d6ff91b9ff3fc0200
David Kesler <thedavidkesler@gmail.com>
2018-04-19 14:41:47 -0500

    server routes complete?

--- server/routes/pizza.router.js
+++ server/routes/pizza.router.js
@@ -4,7 +4,7 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  console.log('made it to router.get')
+  console.log('made it to pizza router.get')
   const queryText = 'SELECT * FROM pizza';
   pool.query(queryText)
     .then((result) => { res.send(result.rows); })
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var port = process.env.PORT || 5000;
 const pizzaRouter = require('./routes/pizza.router');
+const ordersRouter = require('./routes/orders.router');
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
@@ -10,6 +11,7 @@ app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
 app.use('/api/pizza', pizzaRouter);
+app.use('/api/orders', ordersRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
