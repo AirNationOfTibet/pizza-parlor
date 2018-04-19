3e9a7aa5bfcbc2c54c0140e675764aac831d1ba8
David Kesler <thedavidkesler@gmail.com>
2018-04-19 10:53:45 -0500

    initial setup complete

--- src/index.js
+++ src/index.js
@@ -4,5 +4,37 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, call, put} from 'redux-saga/effects';
+import axios from 'axios';
+
+//makes a middleware
+const sagaMiddleware  = createSagaMiddleware();
+
+function* rootSaga () {
+    console.log('rootSaga loaded');
+    yield 1;
+  }
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+
+const storeInstance = createStore(
+    // This function is our first reducer
+    // reducer is a function that runs every time an action is dispatched
+    combineReducers({
+
+
+    }),
+    applyMiddleware(sagaMiddleware, logger)
+);
+
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
+
